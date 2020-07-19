52f2dce2491aa99da6bfe4c6e07d4a476a22a763
Jongbriel <ttyu3@DESKTOP-6LCL9BV>
2020-07-19 17:36:25 +0900

    s

--- src/com/itwill/flight/FlightDAO.java
+++ src/com/itwill/flight/FlightDAO.java
@@ -93,7 +93,7 @@ public class FlightDAO {
 				flight.setFlightFinishDate(updateFlight.getFlightFinishDate());
 				flight.setFlightStartDate(updateFlight.getFlightStartDate());
 				flight.setSeatRating(updateFlight.getSeatRating());
-				
+				 
 				break;
 			}
 		}
