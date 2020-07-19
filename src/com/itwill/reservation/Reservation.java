a41a417cae44ef48909d9bed5d8c829552fedea4
수환잉 <@DESKTOP-68GQP3H>
2020-07-19 15:56:48 +0900

    .

--- src/com/itwill/reservation/Reservation.java
+++ src/com/itwill/reservation/Reservation.java
@@ -1,5 +1,16 @@
 package com.itwill.reservation;
 
-public class Reservation {
+import java.io.Serializable;
+import java.util.Date;
 
+public class Reservation implements Serializable{
+	private String startPoint;			//	Ãâ¹ßÁö
+	private String finishPoint;			//	µµÂøÁö
+	private int passengerNo;			//	½Â°´¼ö
+	private String seatRating;			//	ÁÂ¼®µî±Ş
+	private Date startDate;				//	°¡´Â³¯
+	private Date comingDate;			//	¿À´Â³¯
+	private String passengerType;		//	½Â°´À¯Çü
+	
+	
 }
