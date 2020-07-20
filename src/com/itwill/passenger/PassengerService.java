109eaf9b3d0349d1a2444e65182a955e903db2da
SAMSUNG-PC <SAMSUNG-PC@10.10.5.14>
2020-07-20 16:50:48 +0900

    passengerService

--- src/com/itwill/passenger/PassengerService.java
+++ src/com/itwill/passenger/PassengerService.java
@@ -1,5 +1,23 @@
 package com.itwill.passenger;
 
-public class PassengerService {
+import java.util.ArrayList;
 
+public class PassengerService {
+	
+	private PassengerDAO passengerDAO;
+	public PassengerService()throws Exception{
+		passengerDAO=new PassengerDAO();
+	}
+	
+	//승객 업데이트
+	public void memberUpdate(Passenger updatePassenger) throws Exception{
+		passengerDAO.update(updatePassenger);
+	}
+	
+	//승객 삭제
+	public void memberDelete(String memberId) throws Exception{
+		passengerDAO.delete("memberId");
+		
+	}
 }
+
--- src/com/itwill/reservation/ReservationDAO.java
+++ src/com/itwill/reservation/ReservationDAO.java
@@ -1,5 +1,5 @@
 package com.itwill.reservation;
 
 public class ReservationDAO {
-
+	
 }
