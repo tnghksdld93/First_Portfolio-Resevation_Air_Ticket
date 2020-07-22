9a4a436be7c607dd20354807837fe2807d63bec1
eyoonho54 <eyoonho@naver.com>
2020-07-22 15:57:19 +0900

    commit

--- src/com/itwill/UI/main/Mainframe.java
+++ src/com/itwill/UI/main/Mainframe.java
@@ -32,7 +32,7 @@ public class Mainframe extends JFrame {
 	 */
 	public Mainframe() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 680, 680);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
