8c1431a94624a1e34e3ae6953d6225d43551c5ff
haewoni <suzyhw96@gmail.com>
2020-07-23 11:21:19 +0900

    PassengerUpdate UI

--- src/com/itwill/UI/updatePassenger/JFrameTestMain.java
+++ src/com/itwill/UI/updatePassenger/JFrameTestMain.java
@@ -26,13 +26,13 @@ public class JFrameTestMain extends JFrame {
 			}
 		});
 	}
-
+ 
 	/**
 	 * Create the frame.
 	 */
 	public JFrameTestMain() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 685, 516);
+		setBounds(100, 100, 680, 680);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
